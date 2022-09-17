ab063533e3da8c8f4257e9980304edde06983c68
Sérgio Morais <sergio_a_morais@estudante.sc.senai.br>
2022-09-17 18:15:17 -0300

    updte-exe004

---
+++ aulas/004/exe008.js
@@ -0,0 +1,9 @@
+console.log('O console funcionou corretamente!')
+
+var vel = 12.5
+console.log(`A velocidade do seu carros é ${vel}Km/h.`)
+    if (vel > 60) { // Condição Simples
+        console.log('Você ultrapassou a velocidade permitida. MULTADO')
+    }    
+console.log('Dirija sempre usando cinto de segurança')
+
