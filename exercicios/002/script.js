7fd6cd73cc36c2b7e403d122bad44496a573d55f
Sérgio Morais <sergio_a_morais@estudante.sc.senai.br>
2022-09-19 14:33:17 -0300

    updte-des002

--- exercicios/002/script.js
+++ exercicios/002/script.js
@@ -9,31 +9,34 @@ function carregar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
+
         if(fsex[0].checked) {
             genero = 'Homem'
             if (idade >=0 && idade < 18) {
-                //Jovem
-                img.setAttribute('src', '002\midia\young_man_250')
+                
+                img.setAttribute('src', 'midia/jovem.jpg')
             } else if (idade < 45){
-                //Adulto
-                img.setAttribute('src', '002\midia\man_250')
+                
+                img.setAttribute('src', 'midia/homem.jpg')
             } else {
-                //Idoso
-                img.setAttribute('src', '002\midia\old_man_250')
+                
+                img.setAttribute('src', 'midia/idoso.jpg')
             }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+
             if (idade >=0 && idade < 18) {
-                //Jovem
-                img.setAttribute('src', '002\midia\young_woman_250')
+                
+                img.setAttribute('src', 'midia/menina.jpg')
             } else if (idade < 45){
-                //Adulto
-                img.setAttribute('src', '002\midia\woman_250')
+                
+                img.setAttribute('src', 'midia/mulher.jpg')
             } else {
-                //Idoso
-                img.setAttribute('src', '002\midia\old_woman_250')
+                
+                img.setAttribute('src', 'midia/idosa.jpg')
             }
         }
         res.style.textAlign = 'center'
