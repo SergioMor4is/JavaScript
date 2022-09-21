5fc6ae72471957f9f6fd897ac6879f85a7a7e0af
Sérgio Morais <sergio_a_morais@estudante.sc.senai.br>
2022-09-20 21:53:36 -0300

    ceratexe006

---
+++ aulas/006/exe006.js
@@ -0,0 +1,15 @@
+var c = 1
+while(c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+var d = 1
+do {
+    console.log('Olá! Tudo bem?')
+    d++
+} while(d <= 6)
+
+for(var b = 1; b <= 6; b++){
+    console.log(b)
+}
