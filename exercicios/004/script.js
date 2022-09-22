8bd7777126e0013a0708e7609cec98a08da4bba0
Sérgio Morais <sergio_a_morais@estudante.sc.senai.br>
2022-09-21 23:51:29 -0300

    updte

--- exercicios/004/script.js
+++ exercicios/004/script.js
@@ -2,11 +2,16 @@ function tabuada(){
     var number = document.getElementById('number').value
     var res = document.querySelector('div#res')
     var cont = 0
+    res.innerHTML = ''
+    if (number.value.length == 0) {
+        window.alert('[ERRO] Informe Dados Válidos')
 
-    while (cont <= 10){
+    } else {
+        while (cont <= 10){
         var tab = number * cont
         res.innerHTML += `${number} x ${cont} = ${tab} <br>` 
         
         cont++
+        }
     }
 }
