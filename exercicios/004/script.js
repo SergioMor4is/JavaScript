3e94feb59de8fa6975deedba598ce5f092c3a5f8
Sérgio Morais <sergio_a_morais@estudante.sc.senai.br>
2022-09-21 23:40:23 -0300

    updte

--- exercicios/004/script.js
+++ exercicios/004/script.js
@@ -3,9 +3,9 @@ function tabuada(){
     var res = document.querySelector('div#res')
     var cont = 0
 
-    while (cont < 10){
+    while (cont <= 10){
         var tab = number * cont
-        res.innerHTML += `${number} x ${cont} = ${tab} ` 
+        res.innerHTML += `${number} x ${cont} = ${tab} <br>` 
         
         cont++
     }
